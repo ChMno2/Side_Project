0e0b0ebeb8fb0ac819f0a9365f03156b678e5d16
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-07-11 21:20:10 +0800

    Add files via upload

---
+++ 爬取PTT NBA版上資料/爬取後輸入excel檔案.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import pandas as np
+
+url = "https://www.ptt.cc/bbs/NBA/index.html"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+articles = soup.findAll("div", class_="r-ent")
+data_list = []
+
+for a in articles:
+    data = {}
+    title = a.find("div", class_="title")
+    if title and title.a:
+        title = title.a.text
+    else:
+        title = "沒有標題"
+    data["標題"] = title
+
+    popular = a.find("div", class_="nrec")
+    if popular and popular.span:
+        popular = popular.span.text
+    else:
+        popular = "N/A"
+    data["人氣"] = popular
+
+    date = a.find("div", class_="date")
+    if date:
+        date = date.text.strip()  # 去除日期文字中的多餘空白
+    else:
+        date = "N/A"
+    data["DATE"] = date
+
+    data_list.append(data)
+
+df = pd.DataFrame(data_list)
+df.to_excel("ptt_nba.xlsx", index=False,engine="openpyxl")
+
+
+# print(data_list)
+    # print(f"標題:{title} 人氣:{popular} DATE:{date}")
+
+# # print(response.text)
+# if response.status_code ==200:
+#     with open('output.html','w',encoding ='utf-8') as f:
+#         f.write(response.text)
+#     print("寫入成功")
+# else:
+#     print("沒有抓到網頁")
